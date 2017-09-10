b7db982217bb97dad54f15e6fd754febcf7c787d
zidongchen <zidong.chen@outlook.com>
2017-09-10 17:18:02 +0800

    3.1.8

--- js/loading.js
+++ js/loading.js
@@ -1,13 +1,15 @@
 // JavaScript Document
-  var totalImg = $("img").length-1;
-  var currentImg = 0;
-  $("img").on("load",function(){
-    currentImg++;
-    console.log(currentImg);
-    var progressLoading=currentImg/totalImg*400;
-    console.log(progressLoading);
-    document.getElementById("bar").style.width=progressLoading+"px";
-    if(currentImg === totalImg){
-      $(".loading").fadeOut();
-    }
-  });
+$(document).ready(function(){
+      var totalImg = $("img").length-1;
+      var currentImg = 0;
+      $("img").on("load",function(){
+        currentImg++;
+        console.log(currentImg);
+        var progressLoading=currentImg/totalImg*400;
+        console.log(progressLoading);
+        document.getElementById("bar").style.width=progressLoading+"px";
+        if(currentImg === totalImg){
+          $(".loading").fadeOut();
+        }
+      });
+ });
