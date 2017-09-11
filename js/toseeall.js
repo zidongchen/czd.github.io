c35a7015d02854cd42fc019ed48b65633f569672
zidongchen <zidong.chen@outlook.com>
2017-09-11 13:38:02 +0800

    3.2.0
    更新作品合集的开合方式

---
+++ js/toseeall.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+$(document).ready(function(){
+  $(".btn-all-c4d").on("click",function(){
+    $(".allPicc4d").addClass("active");
+  });
+    $(".btn-close-c4d").on("click",function(){
+    $(".allPicc4d").removeClass("active");
+  });
+});
