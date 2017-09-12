cb05db73b941121b60dbb12e28809cd42ae8574e
zidongchen <zidong.chen@outlook.com>
2017-09-12 13:14:29 +0800

    3.2.9

--- js/toseeall.js
+++ js/toseeall.js
@@ -2,6 +2,7 @@
 $(document).ready(function(){
   $(".btn-all-c4d").on("click",function(){
     $(".allPicc4d").addClass("active");
+    setTimeout(function() {$("img").trigger("sporty");}, 1000);
   });
     $(".btn-close-c4d").on("click",function(){
     $(".allPicc4d").removeClass("active");
