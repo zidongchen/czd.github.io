edbfb7d3735dd29072cf64298b531bb2c488a3ad
zidongchen <zidong.chen@outlook.com>
2018-02-04 16:09:52 +0800

    4.0.1
    微信二维码更新 header动效更新

--- js/main.js
+++ js/main.js
@@ -2,27 +2,32 @@ $(document).ready(function() {
     "use strict";
     var i=0;
     var headerWrapper = document.getElementById("header-wrapper");
+    var navBar=document.getElementById("nav-bar");
 //  鼠标滚轮
     var scrollFunc = function (e) {
         e = e || window.event;
         if (e.wheelDelta) {
-            if (e.wheelDelta / 120 > 0) {
+            if (e.wheelDelta / 120 > 0 && i>0) {
                 i=0;
                 headerWrapper.style.top=0;
+                navBar.style.height=140 + 'px';
             }
-            else if (e.wheelDelta / 120 < 0 && i<=7) {
+            else if (e.wheelDelta / 120 < 0 && i<7) {
                 i++;
-                headerWrapper.style.top=-i*i*2-20+'px';
+                headerWrapper.style.top=-i*13 +'px';
+                navBar.style.height=140-i*i+'px';
             }
         }
         else if (e.detail) {
-            if (e.detail / -3 > 0) {
+            if (e.detail / -3 > 0 && i > 0) {
                 i=0;
                 headerWrapper.style.top=0;
+                navBar.style.height=140+'px';
             }
-            else if (e.detail / 3 < 0 && i<=7) {
+            else if (e.detail / 3 < 0 && i<7) {
                 i++;
-                headerWrapper.style.top=-i*i*2-20+'px';
+                headerWrapper.style.top=-i*13 +'px';
+                navBar.style.height=140-i*i+'px';
             }
         }
     };
@@ -31,4 +36,5 @@ $(document).ready(function() {
         document.addEventListener('DOMMouseScroll', scrollFunc, false);
     }//W3C
     window.onmousewheel = document.onmousewheel = scrollFunc;//IE/Opera/Chrome/Safari
-})
+
+});
