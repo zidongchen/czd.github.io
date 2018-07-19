392c7e947baca98206752f8c766f32d51801b782
zidongchen <zidong.chen@outlook.com>
2018-07-19 20:21:33 +0800

    5.10.4

--- js/contact.js
+++ js/contact.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
   var isMenuClosed = true;
+  var $selfintro;
+  var selfIntroHeight = 0;
   $(".menu-icon-container").on("click", function() {
     if (isMenuClosed) {
       $(".menu-container").addClass("opened");
@@ -12,13 +14,33 @@ $(document).ready(function() {
     }
   });
   $("#hi").on("click", function() {
-    $(".page-writing").addClass("active");
     $("#hi").hide();
-    for (var i = 0; i < $(".selfIntro").length+2; i++) {
-      $(".selfIntro:nth-child(" + i + ")").delay((i - 1) * 2000).animate({
-        opacity: 1,
-      });
+    setTimeout(function() {
+      $(".skip").fadeIn(500);
+    }, 4000);
+    setTimeout(function() {
+      $(".skip").fadeOut(500);
+    }, 24000);
+    setTimeout(function() {
+      $(".pages-container").css("overflow-y","scroll");
+    }, 38000);
+    for (var i = 1; i < $(".selfIntro").length + 1; i++) {
+      $selfintro = $("#selfIntro" + i + "");
+      selfIntroHeight += $selfintro.outerHeight(true);
+      $(".pages-container").animate({
+        scrollTop: selfIntroHeight
+      }, 3000, "linear",$selfintro.delay((i - 1) * 2000).fadeIn(1000));
     }
+
+  });
+  $(".skip").on("click", function() {
+    $(".selfIntro").stop(true);
+    $(".pages-container").stop(true);
+    $(".pages-container").animate({
+      scrollTop: selfIntroHeight},1000);
+    $(".selfIntro").fadeIn();
+    $(".pages-container").css("overflow-y","scroll");
+    $(".skip").fadeOut(500);
   });
 
 });
