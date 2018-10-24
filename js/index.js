e72b07804f29c0555314673200ca5c75998b7d6c
zidongchen <zidong.chen@outlook.com>
2018-10-24 17:04:48 +0800

    7.6.0

--- js/index.js
+++ js/index.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
   function changeIndexPages() {
     if (pageNum <= pages && pageNum >= 1) {
       $(".page-writing").removeClass("active");
-      $(".pages-container").css("transform","translateY(" + -100 * (pageNum - 1) + "vh)");
+      $(".pages-container").css("transform","translateY(" + -33.333 * (pageNum - 1) + "%)");
       $(".page" + pageNum + ">.page-writing").addClass("active");
     }
   }
